27372bd1a96e8542d5b2752cf776d73b53274ec7
biyik <biyik@mstk.mstk>
2022-07-04 03:50:12 +0300

    first commit

---
+++ Pickle_Rick/portal.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rick is sup4r cool</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/bootstrap.min.css">
+  <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Rick Portal</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Commands</a></li>
+        <li><a href="/denied.php">Potions</a></li>
+        <li><a href="/denied.php">Creatures</a></li>
+        <li><a href="/denied.php">Potions</a></li>
+        <li><a href="/denied.php">Beth Clone Notes</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    <form name="input" action="" method="post">
+      <h3>Command Panel</h3></br>
+      <input type="text" class="form-control" name="command" placeholder="Commands"/></br>
+      <input type="submit" value="Execute" class="btn btn-success" name="sub"/>
+    </form>
+    </br><pre>Sup3rS3cretPickl3Ingred.txt
+assets
+clue.txt
+denied.php
+index.html
+login.php
+portal.php
+robots.txt
+</pre>    <!-- Vm1wR1UxTnRWa2RUV0d4VFlrZFNjRlV3V2t0alJsWnlWbXQwVkUxV1duaFZNakExVkcxS1NHVkliRmhoTVhCb1ZsWmFWMVpWTVVWaGVqQT0== -->
+  </div>
+</body>
+</html>
